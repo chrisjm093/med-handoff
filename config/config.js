de883e0e83a71b8d0120ff7950b8861583b888ed
Christopher Marti <chrisjm093@gmail.com>
2020-07-11 13:12:05 -0700

    commit to merge

--- config/config.js
+++ config/config.js
@@ -1,20 +1,20 @@
-module.exports ={
-  "development": {
-    "username": "root",
-    "password": process.env.DEV_DB_PASS,
-    "database": "medhandoff_db",
-    "host": "localhost",
-    "dialect": "mysql"
+module.exports = {
+  development: {
+    username: "root",
+    password: process.env.DEV_DB_PASS,
+    database: "medhandoff_db",
+    host: "localhost",
+    dialect: "mysql"
   },
-  "test": {
-    "username": "obwhekmpqc2wywi1",
-    "password": "tj0l6le2p7azu0ce",
-    "database": "nqdxrsjio1xqczjo",
-    "host": "d6vscs19jtah8iwb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    "dialect": "mysql"
+  test: {
+    username: "obwhekmpqc2wywi1",
+    password: "tj0l6le2p7azu0ce",
+    database: "nqdxrsjio1xqczjo",
+    host: "d6vscs19jtah8iwb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    dialect: "mysql"
   },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
+  production: {
+  use_env_variable: "JAWSDB_URL",
+  dialect: "mysql"
   }
 };
