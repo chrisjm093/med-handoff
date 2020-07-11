49207ce3e8eb698fc4b0d87d9ab2291ef79e7bf7
Nivolving <nivethapartheeban@gmail.com>
2020-07-11 12:57:25 -0700

    Routes Setup

---
+++ controllers/userApiRoute.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const { User } = require("../models");
+
+router.post("/api/users", (req, res) => {
+  User.create({
+    email: req.body.email,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    role: req.body.role,
+    shift: req.body.shift
+  })
+    .then(() => {
+      res.redirect(307, "/api/login");
+    })
+    .catch(err => {
+      res.status(401).json(err);
+    });
+});
+
+module.exports = router;
