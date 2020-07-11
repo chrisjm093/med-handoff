446619de9eb71a0ed4b83dd616cd282041f6fa92
Christopher Marti <chrisjm093@gmail.com>
2020-07-10 21:08:05 -0700

    Button click events functional, redirects to supervisor or practitioner paths

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,15 @@
 $(document).ready(() => {
-  // This file just does a GET request to figure out which user is logged in
+  // This does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
   });
 });
+
+document.getElementById("supervisor-btn").onclick = function() {
+  window.location.replace("/supervisor");
+};
+
+document.getElementById("practitioner-btn").onclick = function() {
+  window.location.replace("/practitioner");
+};
